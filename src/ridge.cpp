eeef3796338bc48e3d019e3ff18f9db5117c5e68
Tim Nugent <timnugent@gmail.com>
2014-08-12 23:24:24 +0100

    added kernel ridge regression code

--- src/ridge.cpp
+++ src/ridge.cpp
@@ -14,19 +14,26 @@ using namespace std;
 class Ridge{
 
 public:
-	Ridge() : lambda(0.001), loaded(0) {}
-	Ridge(const char* data, char sep = ',') : lambda(0.001), loaded(0) {loaded = load_data(data, sep);}
+	Ridge() : loaded(0), kernel_type(1), normalise(0), lambda(0.001), gamma(0.001), constant(1.0), order(2.0) {}
+	Ridge(const char* data, char sep = ',') : loaded(0), kernel_type(1), normalise(0), lambda(0.001), gamma(0.001), constant(1.0), order(2.0)  {loaded = load_data(data, sep);}
 	int load_data(const char* data, char sep);
 	void set_lambda(double l){lambda = l;}
 	void ols(){lambda = 0.0; ridge();}
 	void ridge();
+	double kridge(VectorXd&);
 	void print();
-
+	void set_kernel(const unsigned int i){kernel_type = i;};	
+	void set_normalise(const unsigned int i){normalise = i;};
+	void set_gamma(const double i){gamma = i;};
+	void set_constant(const double i){constant = i;};
+	void set_order(const double i){order = i;};
 private:
-	MatrixXd X;
-	VectorXd Y, B, M, Xscale;
-	double lambda, ymean;
-	int loaded;
+	double kernel(const VectorXd& a, const VectorXd& b);
+	void generate_kernel_matrix();
+	MatrixXd X, K;
+	VectorXd Y, B, M, Xscale, k;
+	unsigned int loaded, kernel_type, normalise;
+	double lambda, ymean, gamma, constant, order;
 
 };
 
@@ -104,6 +111,72 @@ void Ridge::ridge(){
 	}
 }
 
+double Ridge::kridge(VectorXd& x_h){
+
+	if(loaded){
+
+		generate_kernel_matrix();
+		k.resize(Y.rows());
+		for(unsigned int i = 0; i < k.rows(); i++){
+			k(i) = kernel(X.row(i),x_h);
+		}
+		MatrixXd R = MatrixXd::Identity(K.rows(),K.cols());
+		R *= lambda;
+		K += R;
+
+		double y_h = Y.transpose()*K.inverse()*k; 
+		return(y_h);
+
+	}else{
+		cout << "No data loaded!" << endl;
+		return(0);
+	}
+}
+
+double Ridge::kernel(const VectorXd& a, const VectorXd& b){
+
+	switch(kernel_type){
+	    case 2  :
+	    	return(pow(a.dot(b)+constant,order));
+	    default : 
+	    	return(exp(-gamma*((a-b).squaredNorm())));
+	}
+
+}
+
+void Ridge::generate_kernel_matrix(){
+
+	// Fill kernel matrix
+	K.resize(X.rows(),X.rows());
+	for(unsigned int i = 0; i < X.rows(); i++){
+		for(unsigned int j = i; j < X.rows(); j++){
+			K(i,j) = K(j,i) = kernel(X.row(i),X.row(j));
+		}	
+	}
+
+	cout << K << endl;
+
+	// Normalise kernel matrix	
+	if(normalise){
+		VectorXd d = K.rowwise().sum();
+		for(unsigned int i = 0; i < d.rows(); i++){
+			d(i) = 1.0/sqrt(d(i));
+		}
+		auto F = d.asDiagonal();
+		MatrixXd l = (K * F);
+		for(unsigned int i = 0; i < l.rows(); i++){
+			for(unsigned int j = 0; j < l.cols(); j++){
+				l(i,j) = l(i,j) * d(i);
+			}
+		}		
+		K = l;
+
+		cout << endl << "Normalised kernel matrix:" << endl;
+		cout << K << endl;
+	}
+
+}
+
 void Ridge::print(){
 
 	if(loaded){
@@ -133,6 +206,16 @@ int main(int argc, const char* argv[]){
 		R->ridge();
 		R->print();
 	}
+
+
+	cout << endl << "Kernel Ridge Regression:" << endl;
+	R->set_normalise(0);
+	VectorXd x_h(2);
+	x_h(0) = 5.2;
+	x_h(1) = 1.9;
+	double y_h = R->kridge(x_h);
+	cout << "x' = " << endl << x_h << endl;
+	cout << "y' = " << y_h << endl;
 	delete R;
 	return(0);
 
